ded3b41d0791a96c1df7fccd744e08f9b0c50839
ZXCdemon0020 <markykaro@mail.ru>
2024-04-11 20:47:22 +0500

    Initial commit

---
+++ src/hw2/main.java
@@ -0,0 +1,42 @@
+package hw2;
+
+public class main {
+
+
+//    public boolean truefalse (boolean qwerty) {
+//        int a = 5;
+//        int b = 10;
+//        boolean c;
+//        if (a + b > 10 && a + b <= 20);
+//        return qwerty;
+//    }
+
+
+//    public static void main(String[] args) {
+//        int a = -10;
+//        if (a >= 0)
+//            System.out.println("положительное");
+//        else System.out.println("отрицательное");
+//
+//    }
+
+
+//    public boolean truefalse(boolean qwerty) {
+//        int a = -5;
+//        boolean c;
+//        if (a >= 0)
+//            return qwerty;
+//        else return qwerty;
+//        }
+
+
+//    public static void main(String[] args) {
+//        int a = 10;
+//        String str1 = "я сосун";
+//        for (int i = 0; i<a; i++){
+//            System.out.println(str1);
+//        }
+//    }
+
+
+}
